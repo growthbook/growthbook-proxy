92925b7e7f6f5804edc508981ace54c02634b169
Bryce <bryce1@gmail.com>
2023-01-05 10:02:48 -0800

    sample app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import init from "./init";
 import { growthBookProxy } from "./app";
 
+// Sample implementation for the GrowthBookProxy
 (async () => {
   const { app, context } = await init();
+
+  // creating and starting the proxy is a one-liner
   const proxy = await growthBookProxy(app, context);
+
+  // example: use the public interface to do something trivial:
+  setTimeout(() => {
+    console.log( proxy.services.registrar.getAllConnections() );
+  }, 5000);
 })();
