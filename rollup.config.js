fd01bad1035a774069b24ad94690c58e920fd1d2
Bryce <bryce1@gmail.com>
2023-01-11 17:28:31 -0800

    rollup / bundler

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+const extensions = [".js", ".ts", ".tsx", ".jsx"];
+
+export default {
+  input: "src/app.ts",
+  output: [
+    {
+      file: "dist/esm/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/cjs/index.js",
+      format: "cjs",
+      name: "growthbook-proxy",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    json(),
+    resolve({ extensions, jsnext: true }),
+    commonjs(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      preventAssignment: true,
+    }),
+    babel({
+      babelHelpers: "bundled",
+      extensions,
+    }),
+  ],
+};
