19f52bf9c7a9bf0694f7c0ad9494e84269c2b8dd
Bryce <bryce1@gmail.com>
2023-01-20 17:29:10 -0800

    lint

--- src/middleware/cache/refreshStaleCacheMiddleware.ts
+++ src/middleware/cache/refreshStaleCacheMiddleware.ts
@@ -5,7 +5,7 @@ import { featuresCache } from "../../services/cache";
 import logger from "../../services/logger";
 import { eventStreamManager } from "../../services/eventStreamManager";
 
-const scopedPromises: Map<string, Promise<any>> = new Map();
+const scopedPromises: Map<string, Promise<unknown>> = new Map();
 
 export default ({ proxyTarget }: { proxyTarget: string }) =>
   async (req: Request, res: Response) => {
@@ -19,7 +19,7 @@ export default ({ proxyTarget }: { proxyTarget: string }) =>
     let promise = scopedPromises.get(url);
     if (!promise) {
       // eslint-disable-next-line no-async-promise-executor
-      promise = new Promise(async (resolve, reject) => {
+      promise = new Promise(async (resolve) => {
         const resp = await got
           .get(url, {
             headers: { "User-Agent": `GrowthBook Proxy ${version}` },
