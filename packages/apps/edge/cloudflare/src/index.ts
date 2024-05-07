d144c403329ef65981d3395c65c648ab5563f08a
Bryce <bryce1@gmail.com>
2024-05-06 21:58:56 -0700

    cleanup

--- packages/apps/edge/cloudflare/src/index.ts
+++ packages/apps/edge/cloudflare/src/index.ts
@@ -1,9 +1,13 @@
-import { edgeApp } from '@growthbook/edge-utils';
-import init, { Env } from './init';
+import { edgeApp } from "@growthbook/edge-utils";
+import init, { Env } from "./init";
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const context = init(env);
-		return await edgeApp<Request, Response>(context, request) as Response;
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    _: ExecutionContext,
+  ): Promise<Response> {
+    const context = init(env);
+    return (await edgeApp<Request, Response>(context, request)) as Response;
+  },
 };
