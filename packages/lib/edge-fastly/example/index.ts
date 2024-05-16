6deadf93bb2e27e41d8b724ada9700f49ee916c2
Bryce <bryce1@gmail.com>
2024-05-16 00:25:36 -0700

    progress

--- packages/lib/edge-fastly/example/index.ts
+++ packages/lib/edge-fastly/example/index.ts
@@ -1,16 +1,17 @@
 /// <reference types="@fastly/js-compute" />
 import { ConfigStore } from "fastly:config-store";
-import { gbHandleRequest, FastlyConfig, getConfigEnvFromStore } from "packages/lib/edge-fastly/dist";
+import { FastlyConfig, gbHandleRequest, getConfigEnvFromStore } from "@growthbook/edge-fastly";
 
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event: FetchEvent) {
+  const request = event.request;
+
   const envVarsStore = new ConfigStore("env_vars");
   const gbEnv = getConfigEnvFromStore(envVarsStore);
+
   const config: Partial<FastlyConfig> = {
     backends: { "https://www.growthbook.io": "growthbook" }
-  }
-  const res = await gbHandleRequest(event, gbEnv, config);
-  console.log(res)
-  return res;
+  };
+  return await gbHandleRequest(request, gbEnv, config);
 }
--- packages/lib/edge-fastly/src/index.ts
+++ packages/lib/edge-fastly/src/index.ts
@@ -1,18 +1,11 @@
 import { ConfigEnv, edgeApp } from "@growthbook/edge-utils";
 import { init, FastlyConfig } from "./init";
 
-interface FetchEvent {
-  readonly request: Request;
-}
-
 export async function gbHandleRequest(
-  event: FetchEvent,
+  request: Request,
   env?: ConfigEnv,
   config?: Partial<FastlyConfig>,
 ) {
-  const request = event.request;
-  console.log("event", event);
-  console.log("request", request);
   const context = await init(env, config);
   return (await edgeApp<Request, Response>(context, request)) as Response;
 }
