b0f64a3760ef232c3226f3f77c1d6a05514e9ade
Bryce <bryce1@gmail.com>
2024-06-11 23:24:20 -0700

    bump versions

--- packages/lib/edge-fastly/example/build/index.js
+++ packages/lib/edge-fastly/example/build/index.js
@@ -1,24 +1,17 @@
 /// <reference types="@fastly/js-compute" />
 import { ConfigStore } from "fastly:config-store";
+import { KVStore } from "fastly:kv-store";
 import { gbHandleRequest, getConfigEnvFromStore } from "@growthbook/edge-fastly";
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 async function handleRequest(event) {
-    const request = event.request;
     const envVarsStore = new ConfigStore("env_vars");
     const env = getConfigEnvFromStore(envVarsStore);
     const config = {
         apiHostBackend: "api_host",
         backends: { "https://www.growthbook.io": "growthbook" },
         // typically only need to choose one or the other:
-        // gbCacheStore: new KVStore("gb_cache"),
-        // gbPayloadStore: new KVStore("gb_payload"),
+        gbCacheStore: new KVStore("gb_cache"),
+        gbPayloadStore: new KVStore("gb_payload"),
     };
-    try {
-        return await gbHandleRequest(request, env, config);
-    }
-    catch (e) {
-        console.error("error");
-        console.error(e);
-    }
-    return new Response("fail");
+    return await gbHandleRequest(event.request, env, config);
 }
