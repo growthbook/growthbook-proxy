fdaf62bf8385b8967dfa3963c7d052b090d128bd
Bryce <bryce1@gmail.com>
2025-01-06 16:27:27 -0800

    untrack files

--- packages/lib/edge-fastly/dist/helpers.d.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "@growthbook/edge-utils";
-export declare function getRequestURL(req: Request): string;
-export declare function getRequestMethod(req: Request): string;
-export declare function getRequestHeader(req: Request, key: string): string | undefined;
-export declare function sendResponse(ctx: Context<Request, Response>, _?: Response, headers?: Record<string, any>, body?: string, cookies?: Record<string, string>, status?: number): Response;
-export declare function fetchFn(ctx: Context<Request, Response>, url: string): Promise<Response>;
-export declare function proxyRequest(ctx: Context<Request, Response>, req: Request): Promise<Response>;
-export declare function getCookie(req: Request, key: string): string;
-export declare function setCookie(res: Response, key: string, value: string): void;
--- packages/lib/edge-fastly/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { edgeApp } from "@growthbook/edge-utils";
-import { init } from "./init";
-export async function gbHandleRequest(request, env, config) {
-    const context = await init(env, config);
-    return (await edgeApp(context, request));
-}
-export { getConfigEnvFromStore, getKVLocalStoragePolyfill, getPayloadFromKV } from "./init";
-//# sourceMappingURL=index.js.map
